9f9d1d89a6c9d4104860d097b356a6bb2019b9d8
PAZJR93 <omerpaz93@gmail.com>
2020-03-20 19:43:30 +0200

    before adding post

--- facebook/urls.py
+++ facebook/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='facebook-home'),
-    path('', views.create_first_post, name='facebook-create-post'),
+    path('', views.create_post, name='facebook-create-post'),
 
 ]
--- facebook/views.py
+++ facebook/views.py
@@ -7,8 +7,8 @@ def home(request):
     context = {
         'posts' : Post.objects.all()
     }
-    return render(request,'facebook/post.html',context)
+    return render(request,'facebook/feed.html',context)
 
 @login_required
-def create_first_post(request):
-    return render(request,'facebook/create_first_post.html',{'title': 'create post'})
+def create_post(request):
+    return render(request,'facebook/create_post.html',{'title': 'create post'})
