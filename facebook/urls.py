31b486f561a08a12e7b60c070f0ad7e61ef50f31
PAZJR93 <omerpaz93@gmail.com>
2020-03-21 12:39:21 +0200

    now adding to data base comments

--- facebook/urls.py
+++ facebook/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import PostListView,PostDetailView,PostCreateView
 
 urlpatterns = [
-    path('home/', views.home, name='facebook-home'),
+    # path('home/', views.home, name='facebook-home'),
+    path('home/', PostListView.as_view(), name='facebook-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('', views.create_post, name='facebook-create-post'),
 
 ]
--- facebook/views.py
+++ facebook/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView,DeleteView,DetailView,CreateView
 from django.contrib.auth.decorators import login_required
 
 
@@ -8,9 +9,22 @@ def home(request):
     context = {
         'posts' : Post.objects.all()
     }
-    return render(request,'facebook/feed.html',context)
+    return render(request,'facebook/feed.html',{'title': 'feed'})
 
 @login_required
 def create_post(request):
-    return render(request,'facebook/create_post.html',{'title': 'create post'})
+    return render(request,'facebook/post_form.html',{'title': 'create post'})
 
+
+class PostListView(ListView):
+    model = Post
+    template_name  = 'facebook/feed.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['username','status']
